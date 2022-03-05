e73a1b3923cff9f11009aa08b3099bd75fec5228
msaksanen <opml2007@gmail.com>
2022-03-05 13:26:10 +0300

    HW04 Commit

---
+++ homeworks/hw_02/homeworks/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            char ch = 'H';
+            int i = ch;
+            Console.WriteLine($"Hello World! H code - {i}");
+        }
+    }
+}
